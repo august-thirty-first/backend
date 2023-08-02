338b6413d567d1a65e7b799d84dbcfd81a81e85e
chung <cndfuf0159@naver.com>
2023-08-02 11:58:27 +0900

    [refactor] : global pipe validation 설정
    - package.json에 class-transformer 추가
    - main.ts에 app.useGlobalPipes 추가

--- nestjs/src/main.ts
+++ nestjs/src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
@@ -9,6 +11,7 @@ async function bootstrap() {
       credentials: true,
     },
   });
+  app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT);
