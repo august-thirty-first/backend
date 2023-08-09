16cf074bbe75505100a6b402c050bebf2ebc90d0
chung <cndfuf0159@naver.com>
2023-08-09 12:38:28 +0900

    [refactor] passport jwt user 검증
    - user 검증을 passport에서 실행함으로써, 이후에 service에서 처리할 발생할 에러를 방지한다.

--- nestjs/src/passports/passports.module.ts
+++ nestjs/src/passports/passports.module.ts
@@ -3,10 +3,13 @@ import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './jwt.strategy';
 import { FortyTwoStrategy } from './fortytwo.strategy';
 import { TempJwtStrategy } from './tempJwt.strategy';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/auth/entities/User.entity';
+import { UserRepository } from 'src/auth/user.repository';
 
 @Module({
-  imports: [PassportModule.register({})],
-  providers: [JwtStrategy, FortyTwoStrategy, TempJwtStrategy],
+  imports: [PassportModule.register({}), TypeOrmModule.forFeature([User])],
+  providers: [JwtStrategy, FortyTwoStrategy, TempJwtStrategy, UserRepository],
   exports: [PassportModule],
 })
 export class PassportsModule {}
