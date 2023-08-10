fa2b893a383b03b4deb17d730f095f8511bd4313
flowjiyun <gyp2795@gmail.com>
2023-08-10 16:15:40 +0900

    [feat]: 매칭 시스템 구현 - 대기 큐에서 2명을 하나의 방에 입장시키기 - 소켓 연결이 끊겼을 때 대기큐에서 빼기

--- nestjs/src/socket/game/game.gateway.ts
+++ nestjs/src/socket/game/game.gateway.ts
@@ -35,15 +35,30 @@ export class GameGateway
 
   handleDisconnect(client: Socket) {
     console.log(`game socket: ${client.id} disconnected`);
-    this.ladderQueue.pop();
+    this.ladderQueue = this.ladderQueue.filter(
+      element => element.id !== client.id,
+    );
+    console.log(`matching queue length : ${this.ladderQueue.length}`);
   }
 
   @SubscribeMessage('joinQueue')
   handleMessage(client: Socket): void {
     console.log('join queue');
     this.ladderQueue.push(client);
-    this.ladderQueue.forEach(socket => {
-      console.log(socket.id);
-    });
+    console.log(`matching queue pushed : ${this.ladderQueue.length}`);
+    if (this.ladderQueue.length === 2) {
+      const frontSocket = this.ladderQueue[0];
+      const backSocket = this.ladderQueue[1];
+      if (frontSocket.id === backSocket.id) {
+        console.log(`same socket! pop() queue`);
+        this.ladderQueue.pop();
+        console.log(`queue length : ${this.ladderQueue.length}`);
+        return;
+      }
+      backSocket.leave(backSocket.id);
+      backSocket.join(frontSocket.id);
+      this.ladderQueue = [];
+      this.server.to(frontSocket.id).emit('joinGame');
+    }
   }
 }
