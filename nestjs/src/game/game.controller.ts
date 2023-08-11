98f246ca23abe088f4abb02e0fb12ad092edfa99
flowjiyun <gyp2795@gmail.com>
2023-08-11 14:45:51 +0900

    [Feat]: 게임 옵션 선택 데이터 api 생성 - 게임 참여자의 form 입력 데이터를 반환 해주는 api

--- nestjs/src/game/game.controller.ts
+++ nestjs/src/game/game.controller.ts
@@ -1,13 +1,14 @@
 import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { GameService } from './game.service';
+import { GameOptionDto } from './dto/gameOption.dto';
 
 @Controller('game')
 @UseGuards(AuthGuard('jwt'))
 export class GameController {
   constructor(private readonly gameservice: GameService) {}
   @Post('/option')
-  echoOptionData(@Body() data: any): any {
+  echoOptionData(@Body() data: GameOptionDto): GameOptionDto {
     return data;
   }
 }
