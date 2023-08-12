cd4e981f1e0981f63b50ec816c33371250c7a27a
flowjiyun <gyp2795@gmail.com>
2023-08-12 10:49:41 +0900

    [Fix]: Position class에 initialize 메서드 추가

--- nestjs/src/socket/game/class/positon.ts
+++ nestjs/src/socket/game/class/positon.ts
@@ -1,6 +1,11 @@
 export default class Position {
   constructor(public x: number, public y: number) {}
 
+  initialize = (x: number, y: number) => {
+    this.x = x;
+    this.y = y;
+  };
+
   updateX = (value: number) => {
     this.x += value;
   };
