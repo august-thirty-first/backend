3f9e9ddfd1a2ec7d5f01ac7d48bc18003ea481f2
flowjiyun <gyp2795@gmail.com>
2023-08-12 17:02:40 +0900

    [Feat]: GamePlayer class 구현
    - User class를 상속받도록 함

---
+++ nestjs/src/socket/game/class/gamePlayer.ts
@@ -0,0 +1,33 @@
+import { Socket } from 'socket.io';
+import User from './user';
+import { UserStatus } from '../enum/userStatus.enum';
+import Bar from './bar';
+import { PlayerSide } from '../enum/playerSide.enum';
+import { BAR_COLOR } from '../enum/initStatus.enum';
+
+export default class GamePlayer extends User {
+  bar: Bar;
+
+  constructor(
+    socket: Socket,
+    nickName: string,
+    status: UserStatus,
+    public score: number,
+    public side: PlayerSide,
+  ) {
+    super(socket, nickName, status);
+    this.bar = new Bar(side, BAR_COLOR);
+  }
+
+  initializeBar() {
+    this.bar.initializePosition();
+  }
+
+  updateBar(dx: number, dy: number) {
+    this.bar.updatePosition(dx, dy);
+  }
+
+  updateScore(dv: number) {
+    this.score += dv;
+  }
+}
