211d125e31d45086c4c4690bfaf86985fb3c93ac
flowjiyun <gyp2795@gmail.com>
2023-08-12 12:01:32 +0900

    [Feat]: Ball class 구현

---
+++ nestjs/src/socket/game/class/ball.ts
@@ -0,0 +1,19 @@
+import { BallInitPosition } from '../enum/initPosiiton.enum';
+import Position from './positon';
+
+export default class Ball {
+  private position: Position;
+
+  constructor(private readonly color: string) {
+    this.position = new Position();
+  }
+
+  initializePosition(): void {
+    this.position.initialize(BallInitPosition.X, BallInitPosition.Y);
+  }
+
+  updatePosition(dx: number, dy: number): void {
+    this.position.updateX(dx);
+    this.position.updateY(dy);
+  }
+}
