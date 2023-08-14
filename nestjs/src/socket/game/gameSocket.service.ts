f8bb0d5ba5619e484dd50b04609b1206f141c7c2
flowjiyun <gyp2795@gmail.com>
2023-08-14 10:54:53 +0900

    [Fix]: getRoodId 함수 서비스로 이동

--- nestjs/src/socket/game/gameSocket.service.ts
+++ nestjs/src/socket/game/gameSocket.service.ts
@@ -1,1 +1,13 @@
-export class GameSocketService {}
+import { Socket } from 'socket.io';
+
+export class GameSocketService {
+  getRoomId(socket: Socket): string {
+    const rooms: Set<string> = socket.rooms;
+    let roomId: string;
+    for (const item of rooms) {
+      roomId = item;
+      break;
+    }
+    return roomId;
+  }
+}
