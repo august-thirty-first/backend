feee31f1398225f38f2631ba6540536c06225c7c
flowjiyun <gyp2795@gmail.com>
2023-08-15 18:49:25 +0900

    [Chore]: 사용하지 않는 import 삭제

--- nestjs/src/socket/game/class/ball.ts
+++ nestjs/src/socket/game/class/ball.ts
@@ -1,4 +1,3 @@
-import { BallInitPosition } from '../enum/initStatus.enum';
 import getRandomNumberRange from '../utils/getRandomNumberRange';
 import Position from './positon';
 import velocity from './velocity';
