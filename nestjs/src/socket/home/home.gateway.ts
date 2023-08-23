243d6be391775933d9b50bca2ba413b1608ee9d3
yoonkiung <rldnd4429@gmail.com>
2023-08-23 09:45:53 +0900

    [Feat]: leaveRoom 이벤트 생성 - 모든 room에서 나가는 로직을 함수로 변경 - DM시에도 room에서 나가주게 설정

--- nestjs/src/socket/home/home.gateway.ts
+++ nestjs/src/socket/home/home.gateway.ts
@@ -93,6 +93,7 @@ export class HomeGateway
     );
     //서로 block이 된 상태인지 확인하는 로직 필요
     if (targetSocket) {
+      this.handleLeaveAllRoom(client);
       targetSocket.emit(
         'directMessage',
         `${client['nickname']}: ${directMessageDto.inputMessage}`,
@@ -201,20 +202,25 @@ export class HomeGateway
     console.log(payload);
   }
 
-  @SubscribeMessage('enterRoom')
-  handleEnterRoom(
-    @ConnectedSocket() client: Socket,
-    @MessageBody() payload: string,
-  ) {
+  @SubscribeMessage('leaveAllRoom')
+  handleLeaveAllRoom(@ConnectedSocket() client: Socket) {
     const currentRooms = client.rooms;
-    const roomIdDto: RoomIdDto = JSON.parse(payload);
-
     for (const room of currentRooms) {
       if (room !== client.id) {
         client.leave(room);
         console.log(`leave Room: ${room}`);
       }
     }
+  }
+
+  @SubscribeMessage('enterRoom')
+  handleEnterRoom(
+    @ConnectedSocket() client: Socket,
+    @MessageBody() payload: string,
+  ) {
+    const roomIdDto: RoomIdDto = JSON.parse(payload);
+
+    this.handleLeaveAllRoom(client);
     client.join(roomIdDto.roomId);
     console.log(`join Room: ${roomIdDto.roomId}`);
     client.emit('roomChange', roomIdDto.roomId);
