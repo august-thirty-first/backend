3090f4ace7d092b92c9a74b96f1f9a9a9deaabb7
chung <cndfuf0159@naver.com>
2023-08-31 18:13:24 +0900

    [feat] typeorm 설정 추가
    - typeorm의 migration 설정을 해줬습니다.

--- nestjs/src/configs/typeorm.config.ts
+++ nestjs/src/configs/typeorm.config.ts
@@ -1,12 +1,30 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 export const typeORMConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: 'postgres',
   port: 5432,
-  username: process.env.PONG_USER,
-  password: process.env.PONG_PASSWORD,
-  database: process.env.PONG_DATABASE,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
-  synchronize: true,
+  synchronize: false,
 };
+
+const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: 'postgres',
+  port: 5432,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/../**/migrations/*.ts'],
+  migrationsTableName: 'migrations',
+  synchronize: false,
+});
+
+export default AppDataSource;
